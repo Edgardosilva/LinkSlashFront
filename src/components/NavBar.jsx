df30de571dafa2373e3baff1d3ebf596a394822a
Edgardosilva <edgardosilva.es@gmail.com>
2024-06-11 13:19:57 -0400

    Update NavBar component links to open in new tab

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,9 +4,9 @@ const NavBar = () => {
     return (
         <nav className='border-b-2 flex justify-center items-center h-20'>
             <ul className='flex justify-center gap-10'>
-                <li><a href="https://www.linkedin.com/in/edgardo-silva/">Linkedin</a></li>
-                <li><a href="https://github.com/Edgardosilva">GitHub</a></li>
-                <li><a href="https://github.com/Edgardosilva/LinkSlashFront">Repositorio</a></li>
+                <li><a href="https://www.linkedin.com/in/edgardo-silva/" target='_blank'>Linkedin</a></li>
+                <li><a href="https://github.com/Edgardosilva" target='_blank'>GitHub</a></li>
+                <li><a href="https://github.com/Edgardosilva/LinkSlashFront" target='_blank'>Repositorio</a></li>
             </ul>
         </nav>
     );
